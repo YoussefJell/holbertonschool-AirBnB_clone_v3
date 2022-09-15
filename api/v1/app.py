a2631c8c0da4e2759aaa3c0ecfb8f4a33f70a16c
YoussefJell <youssef.jellouli@yahoo.com>
2022-09-15 18:24:12 +0000

    add

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """starts a Flask web application"""
 from flask import Flask
-from models.__init__ import storage
+from models import storage
 from api.v1.views import app_views
 from os import getenv
 
