ac36649902e2b60c42f7d996765f5428b1d6c976
YoussefJell <youssef.jellouli@yahoo.com>
2022-09-16 09:26:12 +0000

    Task 5 complete

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,13 @@ route that returns json status response
 """
 from api.v1.views import app_views
 from flask import jsonify, request
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
@@ -13,3 +20,17 @@ def send_status():
     if request.method == "GET":
         response = {'status': 'OK'}
     return jsonify(response)
+
+
+@app_views.route('/stats')
+def show_stats():
+    """returns the count for all objects"""
+    all_counts = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return jsonify(all_counts)
